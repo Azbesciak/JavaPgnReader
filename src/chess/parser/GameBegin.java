8ce5b393c10dce48c947aa3890b9277706b12bd4
Witold Kupś <witold.kups@interlan.pl>
2020-02-15 19:24:55 +0100

    Add original sources

---
+++ src/chess/parser/GameBegin.java
@@ -0,0 +1,12 @@
+package chess.parser;
+
+/**
+ * Created 06.10.2017.
+ */
+
+public class GameBegin extends Entity {
+    @Override
+    public String toString() {
+        return "GameBegin";
+    }
+}
